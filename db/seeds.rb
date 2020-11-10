54b27c6cf9113a5746efbeb84d8ad67cbcfddd86
kurogi yuki <xtoriymoonz2@yahoo.co.jp>
2020-11-10 15:35:40 +0900

    db/seed.rbにテスト用管理者ユーザーを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,8 @@ User.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORD
   puts 'ユーザーの初期データインポートに成功しました。'
 end
-AdminUser.find_or_create_by!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+AdminUser.find_or_create_by!(email: 'admin@example.com') do |adminuser|
+  adminuser.password = "password"
+  adminuser.password_confirmation = "password"
+end
