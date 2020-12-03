73279ae1a47bffdf33f3e1657970f547b805d6b8
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2020-12-03 15:15:12 +0900

    Lineモデル生成

---
+++ db/migrate/20201203061421_create_lines.rb
@@ -0,0 +1,10 @@
+class CreateLines < ActiveRecord::Migration[6.0]
+  def change
+    create_table :lines do |t|
+      t.string :genre
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
