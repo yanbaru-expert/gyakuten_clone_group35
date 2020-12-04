769aa620edbf2a606290891e6b5a25b72ce2abd5
atsushi0919 <hatake0919@yahoo.co.jp>
2020-12-04 11:39:42 +0900

    読破済みテーブルに従いボタン表示切り替え

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,6 +1,8 @@
 class TextsController < ApplicationController
   def index
     @texts = Text.all
+    # current_user.id で readed_text の text_id を絞り込み @readed_text_ids に格納
+    @readed_text_ids = ReadedText.where(user_id: current_user.id).pluck(:text_id)
   end
 
   def show
