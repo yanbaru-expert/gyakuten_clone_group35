1ff3a69086adcf8275de4c452b6a33200c30e000
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2021-01-05 13:42:09 +0900

    不要アクション削除

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -6,31 +6,6 @@ class LinesController < ApplicationController
   def show
     @line = Line.find(params[:id])
   end
-
-  def new
-    @line = Line.new
-  end
-    
-  def create
-    line = Line.create!(line_params)
-    redirect_to line
-  end
-   
-  def edit
-    @line = Line.find(params[:id])
-  end
-    
-  def update
-    line = Line.find(params[:id])
-    line.update!(line_params)
-    redirect_to line
-  end
-
-  def destroy
-    line = Line.find(params[:id])
-    line.destroy!
-    redirect_to root_path
-  end
   
 end
     
